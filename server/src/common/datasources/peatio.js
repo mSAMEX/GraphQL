7dd7d52912e04ed541cf241a975d8859fad3593d
Mehdi Aghazadeh <65454478+mehdi-agh@users.noreply.github.com>
2023-05-21 08:56:36 +0300

    Update peatio.js

--- server/src/common/datasources/peatio.js
+++ server/src/common/datasources/peatio.js
@@ -8,7 +8,7 @@ class PeatioAPI extends RESTDataSource {
 	constructor() {
 		super();
 		this.baseURL = config.peatioURL;
-		this.internalURL = config.peatioInternalURL || 'https://tr.msamex.com/api/v2/';
+		this.internalURL = config.peatioInternalURL || 'https://tr.msamex.com/api/v2/peatio/';
 	}
 
 	async getAllCurrencies() {
