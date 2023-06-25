0559fe4aef0caf8587ed4a1fd91a81d70371f2da
Mehdi Aghazadeh <65454478+mehdi-agh@users.noreply.github.com>
2023-06-26 01:19:00 +0300

    Update peatioAdmin.js

--- server/src/common/datasources/peatioAdmin.js
+++ server/src/common/datasources/peatioAdmin.js
@@ -8,7 +8,7 @@ class PeatioAdminAPI extends RESTDataSource {
 	constructor() {
 		super();
 		this.baseURL = config.peatioURL;
-		this.internalURL = config.peatioInternalURL || 'https://tr.msamex.com/api/v2/peatio/';
+		this.internalURL = config.peatioInternalURL || 'https://www.msamex.com/api/v2/peatio/';
 	}
 
 	getPaginationHeaders(response) {
