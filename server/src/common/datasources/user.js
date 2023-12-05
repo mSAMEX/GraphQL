ec48a6f9cf129a7008066b181ba72c7d9281c4af
Mehdi Aghazadeh <65454478+mehdi-agh@users.noreply.github.com>
2023-12-06 00:49:00 +0300

    Update user.js

--- server/src/common/datasources/user.js
+++ server/src/common/datasources/user.js
@@ -3,6 +3,7 @@ var setCookie = require('set-cookie-parser');
 const { RESTDataSource } = require('apollo-datasource-rest');
 const config = require('../../../config.json');
 const paginationFromHeader = require('../../helpers/getPagination');
+const ip = require('ipware')().get_ip(req)?.clientIp || req?.ip || req?.socket?.remoteAddress || '127.0.0.1'
 
 class UserAPI extends RESTDataSource {
 	constructor() {
@@ -20,7 +21,8 @@ class UserAPI extends RESTDataSource {
 			referrer: 'no-referrer',
 			headers: {
 				'Content-Type': 'application/json;charset=utf-8',
-				'User-Agent': 'Exchange/proxy'
+				'User-Agent': 'Exchange/App'
+				'User-Ip': ip
 			},
 			body: JSON.stringify({
 				email,
